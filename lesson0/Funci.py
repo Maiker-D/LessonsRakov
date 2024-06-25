a09b89a22f08ddf5491d53de76f8e82729a63d07
Rakov <rakov.d91@mail.ru>
2024-06-25 15:36:30 +0300

    test commit

---
+++ lesson0/Funci.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
