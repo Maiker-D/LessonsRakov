39a1dea4b18a4ccfbf5f66afe9a706f014ec6b14
Rakov <rakov.d91@mail.ru>
2024-06-25 13:33:44 +0300

    test commit

--- lesson0/Style.py
+++
@@ -1,9 +0,0 @@
-while True:
-    number = int(input("Введите число:"))
-    if number % 2 == 0:
-        print("Число четное")
-        continue
-    else:
-        print("Число нечетное")
-    print('Меня не забыли')
-print('Я за циклом')
