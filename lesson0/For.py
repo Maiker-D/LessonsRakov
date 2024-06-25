e577da1b3cc2c25c11878be37c97631cf156f3a3
Rakov <rakov.d91@mail.ru>
2024-06-25 14:52:29 +0300

    test commit

---
+++ lesson0/For.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for num in numbers:
+    if num > 1:
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        else:
+            not_primes.append(num)
+print(primes)
+print(not_primes)
