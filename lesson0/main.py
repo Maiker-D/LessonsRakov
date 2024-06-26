ae90e4fd6fedabd0a101627325701436ddcff1a7
Rakov <rakov.d91@mail.ru>
2024-06-26 20:35:22 +0300

    test commit

--- lesson0/main.py
+++ lesson0/main.py
@@ -1,7 +1,14 @@
-from time import sleep
-
-a = 5
-print(a)
-print('Я тут')
-sleep(4)
-print('Фух, 4 секунды прошло')
+def get_password(n):
+    used_pairs = set()
+    result = ""
+    for i in range(1, 21):
+        for j in range(i+1, 21):
+            if n % (i +j) == 0:
+                pair = str(i) + str(j)
+                if pair not in used_pairs:
+                    used_pairs.add(pair)
+                    result += pair
+    return result
+n = int(input("Введите число от 3 до 20: "))
+password = get_password(n)
+print(f"Пароль для числа {n}: {password}")
