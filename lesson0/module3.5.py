2bd89241370a7ecccdfda2d58071692cb9d484ed
Rakov <rakov.d91@mail.ru>
2024-07-22 16:46:16 +0300

    test commit

---
+++ lesson0/module3.5.py
@@ -0,0 +1,10 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) == 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+number = 40203
+result = get_multiplied_digits(number)
+print(result)
